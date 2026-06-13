ec36fd4752e58d0b23005c1677f2594a39de1110
Gayna Chawla <gaynachawla@gmail.com>
2026-06-14 00:01:53 +0530

    main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,15 @@
 
 #include <cstdio>
 #define GL_SILENCE_DEPRECATION
+#include "include/glew.h"
 
+#include <OpenGL/gl.h>
+#include "include/glfw3.h"
+#include "GLUT/glut.h"
+#include "OpenGL/glu.h"
 
 #include <cstdlib>
-
+#include "Cube.h"
 #include <thread>
 #include <atomic>
 #include <chrono>
