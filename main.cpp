83c5af6c38c3321c88c0852ca87a1e2336c56f48
Gayna Chawla <gaynachawla@gmail.com>
2026-06-14 00:00:40 +0530

    main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <map>
+
+#include <cstdio>
+#define GL_SILENCE_DEPRECATION
+
+
+#include <cstdlib>
+
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+#include <random>
+#include <ctime>
