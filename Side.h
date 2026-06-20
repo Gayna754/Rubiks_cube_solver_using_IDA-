9153e9bd062d14567f2ebe40e0718523b6b86284
Gayna Chawla <gaynachawla@gmail.com>
2026-06-20 23:25:30 +0530

    side header file

---
+++ Side.h
@@ -0,0 +1,19 @@
+
+#ifndef RUBIKS_CUBE_SIDE_H
+#define RUBIKS_CUBE_SIDE_H
+
+#include "iostream"
+#include "EdgePiece.h"
+#include "Colour.h"
+#include "CornerPiece.h"
+#include "CentrePiece.h"
+
+class Side {
+public:
+    Side() = default;
+    std::vector <EdgePiece> edgePieces;
+    
+};
+
+
+#endif //RUBIKS_CUBE_SIDE_H
