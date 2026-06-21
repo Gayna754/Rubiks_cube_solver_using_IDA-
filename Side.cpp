22890295dcfa170c0bf60f2af5923644bb71cb97
Gayna Chawla <gaynachawla@gmail.com>
2026-06-21 23:39:24 +0530

    Solver

--- Side.cpp
+++ Side.cpp
@@ -1,1 +1,10 @@
 #include "Side.h"
+
+
+bool Side::allSameColours() {
+    for (auto edgePiece: edgePieces) {
+        if (edgePiece.colour.colourName != centrePiece.colour.colourName) {
+            return false;
+        }
+    }
+}
