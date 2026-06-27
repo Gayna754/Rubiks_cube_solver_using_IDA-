910d59a8c8e915f63ddd788914650ca8dc38b8fe
Gayna Chawla <gaynachawla@gmail.com>
2026-06-27 23:46:05 +0530

    pieces

--- Pieces.h
+++ Pieces.h
@@ -1,6 +1,5 @@
-
-
-
+#ifndef RUBIKS_CUBE_PIECES_H
+#define RUBIKS_CUBE_PIECES_H
 
 #include <iostream>
 #include "Colour.h"
@@ -10,3 +9,6 @@ public:
     std::vector<Colour> colours;
     Pieces() = default;
 };
+
+
+#endif //RUBIKS_CUBE_PIECES_H
