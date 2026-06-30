ae4227c9692f962dbbf21eef01b8a1bd02b4e2b2
Gayna Chawla <gaynachawla@gmail.com>
2026-06-30 23:37:02 +0530

    corner

--- CornerPiece.h
+++ CornerPiece.h
@@ -1,8 +1,24 @@
 
+
 #define RUBIKS_CUBE_CORNERPIECE_H
-
-
 
 #include "iostream"
 #include "Colour.h"
 #include "Pieces.h"
+
+class CornerPiece : public Pieces {
+public:
+    Colour colour;
+    Colour topColour;
+    Colour sideColour;
+
+    std::vector<Colour> colours;
+
+    CornerPiece() = default;
+
+    CornerPiece(Colour colourFront, Colour colourTop, Colour colourSide);
+
+    CornerPiece top();
+
+    CornerPiece side();
+};
