53df6ec13330c168f61e329d4e275c25284682f0
Gayna Chawla <gaynachawla@gmail.com>
2026-07-01 23:27:20 +0530

    cube

--- Cube.cpp
+++ Cube.cpp
@@ -1,3 +1,4 @@
+
 #include "Cube.h"
 
 #include <random>
@@ -5,3 +6,10 @@
 #include <utility>
 #include <cmath>
 #include <unordered_set>
+
+  std::unordered_set<std::string> visitedStates;
+
+// Coordinates below for calculating the heuristic value 
+int sideOneCoordinates[][3] = {{1, 3, 3},{2, 3, 3},{3, 3, 3},
+                               {1, 2, 3},{2, 2, 3},{3, 2, 3},
+                               {1, 1, 3},{2, 1, 3},{3, 1, 3}};
