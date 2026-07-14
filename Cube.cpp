f4e04ec376e9bca29982c13f1c47a8ffeec3ef73
Gayna Chawla <gaynachawla@gmail.com>
2026-07-14 23:07:20 +0530

    cube

--- Cube.cpp
+++ Cube.cpp
@@ -1203,7 +1203,7 @@ int* Cube::findCoordinate(int sideIndex, int pieceIndex, int isCornerOrEdge) {
         else if (pieceIndex == 2 and isCornerOrEdge == 1) {
             return sideSixCoordinates[7];
         }
-        else if (pieceIndex == 12 and isCornerOrEdge == 1) {
+        else if (pieceIndex == 13 and isCornerOrEdge == 1) {
             return sideSixCoordinates[3];
         }
     }
